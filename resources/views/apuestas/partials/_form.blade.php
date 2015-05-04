a285481e3df58af789b55e92defa1dbe79ec4bb6
Salvador P. <salvadorperezd@gmail.com>
2015-05-04 14:02:17 +0200

    Added class to the select field

--- resources/views/apuestas/partials/_form.blade.php
+++ resources/views/apuestas/partials/_form.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
   {!! Form::label('users_id', 'Usuario:', array('class' => 'col-sm-2 control-label', 'for' => 'users_id')) !!}
-  <div class = "col-sm-10">{!! Form::select('users_id', $users, !empty($apuesta) ? $apuesta->users_id : '') !!}</div>
+  <div class = "col-sm-10">{!! Form::select('users_id', $users, !empty($apuesta) ? $apuesta->users_id : '', ['class' => 'form-control']) !!}</div>
 </div>
 <div class="form-group">
   {!! Form::label('total', 'Total:', array('class' => 'col-sm-2 control-label', 'for' => 'total')) !!}
