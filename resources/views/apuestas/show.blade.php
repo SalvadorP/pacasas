8fe7d2177271df476efbe9156c7379bd043fff9c
Salvador P. <salvadorperezd@gmail.com>
2015-05-04 14:18:58 +0200

    Added normal curly braces not the escaping content ones.

--- resources/views/apuestas/show.blade.php
+++ resources/views/apuestas/show.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
     <h2>{{ $apuesta->total }}</h2>
-
     <p>
-        {!! link_to_route('apuestas.index', 'volver a las Apuestas') !!} |
-        {!! link_to_route('apuestas.create', 'Crear Apuesta', $apuesta->slug) !!}
+        {{ link_to_route('apuestas.index', 'volver a las Apuestas') }} |
+        {{ link_to_route('apuestas.create', 'Crear Apuesta', $apuesta->slug) }}
     </p>
 @endsection
