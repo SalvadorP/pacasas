76c9292638ddd60fec1d7671e6bb60acd73ee730
Salvador P. <salvadorperezd@gmail.com>
2015-05-04 14:16:30 +0200

    Added normal curly braces not the escaping content ones.

--- resources/views/apuestas/edit.blade.php
+++ resources/views/apuestas/edit.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     <h2>Editar Apuesta</h2>
-
-    {!! Form::model($apuesta, ['method' => 'PATCH', 'route' => ['apuestas.update', $apuesta->slug]]) !!}
+    {{ Form::model($apuesta, ['method' => 'PATCH', 'route' => ['apuestas.update', $apuesta->slug]]) }}
         @include('apuestas/partials/_form', ['submit_text' => 'Edit Apuesta'])
-    {!! Form::close() !!}
+    {{ Form::close() }}
 @endsection
